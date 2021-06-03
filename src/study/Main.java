2f3589464c1aa13f6f175cdd73ad0607709f1959
김동진 <rlaehdwls310@khu.ac.kr>
2021-06-03 14:17:09 +0000

    21.06.03. class 사용

--- src/study/Main.java
+++ src/study/Main.java
@@ -1,7 +1,29 @@
 package study;
+import java.io.IOException;
+
+class Car{
+    private String modelName;
+    private int modelYear;
+    private String color;
+    private int maxSpeed;
+    private int currentSpeed;
+    
+    Car(String modelName, int modelYear, String color, int maxSpeed){
+        this.modelName = modelName;
+        this.modelYear = modelYear;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.currentSpeed = 0;
+    }
+    
+    public String getModel(){
+        return this.modelYear + "년식 " + this.modelName + " " + this.color;
+    }
+}
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello world!");
+        Car myCar = new Car("아반떼",2017, "흰색", 200);
+        System.out.println(myCar.getModel());
     }
 }
